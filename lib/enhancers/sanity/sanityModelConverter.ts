190ecb2c3af1f615f7f050bdcf52696e0073f8bb
Robert van den Boorn <robertb@uniform.dev>
2023-01-12 19:23:06 +0100

    Do not initialize Sanity client if it is not configured in the .env variables.

--- lib/enhancers/sanity/sanityModelConverter.ts
+++ lib/enhancers/sanity/sanityModelConverter.ts
@@ -6,18 +6,12 @@ import getConfig from 'next/config';
 const { serverRuntimeConfig } = getConfig();
 const { sanityProjectId, sanityCdnProjectId, sanityDataset, sanityUseCdn, sanityApiVersion } = serverRuntimeConfig;
 
-const client = new createSanityClient({
-  projectId: sanityCdnProjectId,
-  dataset: sanityDataset,
-  useCdn: sanityUseCdn,
-  apiVersion: sanityApiVersion
-});
+const sanityConfigured: boolean = 
+  sanityProjectId !== undefined && sanityCdnProjectId !== undefined && sanityDataset !== undefined && sanityUseCdn !== undefined && sanityApiVersion !== undefined;
 
 export const sanityModelConverter = ({ component, parameter }: { component: ComponentInstance; parameter: any }) => {
 
     if (component.type === 'HeroSanity') {
-        
-        const builder = imageUrlBuilder(client);
         
         const returnValue = {
             title: parameter?.value?.title || '', 
@@ -25,12 +19,22 @@ export const sanityModelConverter = ({ component, parameter }: { component: Comp
             buttonText: parameter?.value?.buttonText || '', 
             buttonLink: parameter?.value?.buttonLinkSug || '', 
             image: {
-                src: builder.image(parameter?.value?.image).url().replace(sanityCdnProjectId, sanityProjectId) || '',
+                src: '',
                 alt: '',
             }
         };
 
-        console.log(JSON.stringify(returnValue, null, 2))
+        if (sanityConfigured) {
+            const client = new createSanityClient({
+                projectId: sanityCdnProjectId,
+                dataset: sanityDataset,
+                useCdn: sanityUseCdn,
+                apiVersion: sanityApiVersion
+              });
+
+              const builder = imageUrlBuilder(client);
+              returnValue.image.src = builder.image(parameter?.value?.image).url().replace(sanityCdnProjectId, sanityProjectId);
+        }
 
         return returnValue;    
     }
