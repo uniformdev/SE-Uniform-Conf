412e4c3811e76344de77a9294993ca60e1c8e4c1
Robert van den Boorn <robertb@uniform.dev>
2023-01-13 11:04:03 +0100

    Fixed document formatting.

--- lib/enhancers/hygraph/hygraphModelConverter.ts
+++ lib/enhancers/hygraph/hygraphModelConverter.ts
@@ -1,20 +1,24 @@
-import { ComponentInstance } from '@uniformdev/canvas';
+import { ComponentInstance } from "@uniformdev/canvas";
 
-export const hygraphModelConverter = ({ component, parameter }: { component: ComponentInstance; parameter: any }) => {
+export const hygraphModelConverter = ({
+	component,
+	parameter,
+}: {
+	component: ComponentInstance;
+	parameter: any;
+}) => {
+	if (component.type === "HeroHygraph") {
+		const returnValue = {
+			title: parameter?.value[0]?.title || "",
+			description: parameter?.value[0]?.description || "",
+			buttonText: parameter?.value[0]?.buttonText || "",
+			buttonLink: parameter?.value[0]?.buttonLinkSlug || "",
+			image: {
+				src: parameter?.value[0]?.image?.url || "",
+				alt: "",
+			},
+		};
 
-    if (component.type === 'HeroHygraph') {
-    
-        const returnValue = {
-            title: parameter?.value[0]?.title || '', 
-            description: parameter?.value[0]?.description || '', 
-            buttonText: parameter?.value[0]?.buttonText || '', 
-            buttonLink: parameter?.value[0]?.buttonLinkSlug || '', 
-            image: {
-                src: parameter?.value[0]?.image?.url || '',
-                alt: '',
-            }
-        };
-
-        return returnValue;    
-    }
-}
+		return returnValue;
+	}
+};
