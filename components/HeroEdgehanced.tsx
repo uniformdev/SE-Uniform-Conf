05fb23f279eabff9e726cfefde0ec931ec333f5d
John Running <john.a.running@gmail.com>
2023-01-18 15:43:25 -0500

    deconstructed props vars

--- components/HeroEdgehanced.tsx
+++ components/HeroEdgehanced.tsx
@@ -11,6 +11,7 @@ export type HeroProps = {
 };
 
 export function HeroEdgehanced(props: HeroProps) {
+  const {title, description, buttonText, buttonLink, image, imageAltText} = props
   return (
     <>
       <div className="pt-24">
@@ -18,30 +19,30 @@ export function HeroEdgehanced(props: HeroProps) {
           <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left min-h-500">
             <h1
               className="my-4 text-5xl font-bold leading-tight"
-              dangerouslySetInnerHTML={{ __html: props?.title }}
+              dangerouslySetInnerHTML={{ __html: title }}
             />
             <p
               className="leading-normal text-2xl mb-8"
-              dangerouslySetInnerHTML={{ __html: props?.description }}
+              dangerouslySetInnerHTML={{ __html: description }}
             />
-            {props?.buttonText ? (
+            {buttonText ? (
               <Link
                 prefetch={false}
-                href={props?.buttonLink}
+                href={buttonLink}
               >
                 <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">
-                  {props?.buttonText}
+                  {buttonText}
                 </button>
               </Link>
             ) : null}
           </div>
           <div className="w-full md:w-3/5 py-6 text-center">
-            {props?.image && (
+            {image && (
               <img
                 className="w-full md:w-4/5 z-50 min-h-500 max-h-500"
                 height={500}
-                src={props?.image}
-                alt={props?.imageAltText}
+                src={image}
+                alt={imageAltText}
               />
             )}
           </div>
