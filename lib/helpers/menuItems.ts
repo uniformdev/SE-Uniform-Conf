af99dbadcc2a8fb65b250a98796bc65d76851fdb
Robert van den Boorn <robertb@uniform.dev>
2023-01-30 16:15:59 +0100

    chore: formatting

--- lib/helpers/menuItems.ts
+++ lib/helpers/menuItems.ts
@@ -2,24 +2,23 @@ import { projectMapClient } from "lib/projectMapClient";
 import getConfig from "next/config";
 
 const {
-    serverRuntimeConfig: { projectMapId },
-  } = getConfig();
-  
+	serverRuntimeConfig: { projectMapId },
+} = getConfig();
+
 export async function GetMenuItems() {
-    const tree = await projectMapClient.getSubtree({
-      projectMapId, 
-      depth: 1
-    });
-  
-    if (tree?.children?.length) {
-      let childItems: Array<{ name: string, url: string }> = [];
-  
-      tree.children.forEach(childItem => {
-        childItems.push({ name: childItem.name, url: childItem.path });
-      });
-  
-      return childItems;
-    }
-  
-    return [];
-  }
+	const tree = await projectMapClient.getSubtree({
+		projectMapId,
+		depth: 1,
+	});
+
+	if (tree?.children?.length) {
+		let childItems: Array<{ name: string; url: string }> = [];
+		tree.children.forEach((childItem) => {
+			childItems.push({ name: childItem.name, url: childItem.path });
+		});
+
+		return childItems;
+	}
+
+	return [];
+}
