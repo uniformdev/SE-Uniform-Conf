2a3f5b5b4fa4f3a9524d7f2da7b9c2acb46920c1
Robert van den Boorn <robertb@uniform.dev>
2023-02-03 22:44:57 +0100

    Escaped missing content for Strapi.

--- components/HeroStrapi.tsx
+++ components/HeroStrapi.tsx
@@ -25,7 +25,7 @@ export type HeroProps = {
 
 export function HeroStrapi(props: HeroProps) {
   const locale = useContext(LocaleContext);
-  const localizedProps = locale === LOCALE_ENGLISH_UNITED_STATES ? props : props.Entry.localizations.filter(l => l.Entry.locale === locale)[0];
+  const localizedProps = locale === LOCALE_ENGLISH_UNITED_STATES ? props : props?.Entry?.localizations?.filter(l => l?.Entry?.locale === locale)[0];
 
   return (
     <>
@@ -33,11 +33,11 @@ export function HeroStrapi(props: HeroProps) {
         <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
           <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left min-h-500">
             <h1 className="my-4 text-5xl font-bold leading-tight">{localizedProps?.Entry?.title}</h1>
-            <p className="leading-normal text-2xl mb-8">{localizedProps.Entry?.description}</p>
-            {localizedProps.Entry?.buttonText && localizedProps.Entry.buttonLink ? (
-              <Link prefetch={false} href={localizedProps.Entry ? localizedProps.Entry?.buttonLink : "#"}>
+            <p className="leading-normal text-2xl mb-8">{localizedProps?.Entry?.description}</p>
+            {localizedProps?.Entry?.buttonText && localizedProps?.Entry.buttonLink ? (
+              <Link prefetch={false} href={localizedProps?.Entry ? localizedProps?.Entry?.buttonLink : "#"}>
                 <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">
-                  {localizedProps.Entry?.buttonText}
+                  {localizedProps?.Entry?.buttonText}
                 </button>
               </Link>
             ) : null}
