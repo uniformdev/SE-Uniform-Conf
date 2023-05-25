8e7cce8917c8d501f502e3cb0973619dd508b632
Robert van den Boorn <robertb@uniform.dev>
2023-05-25 22:35:41 +0200

    Added variable margin settings to the TalkList component.

--- src/components/TalkList.tsx
+++ src/components/TalkList.tsx
@@ -2,6 +2,32 @@
 import { UniformSlot, UniformText } from "@uniformdev/canvas-react";
 
 /**
+ Interface for the topMarginMap object.
+ 
+ @interface TopMarginMap
+*/
+interface TopMarginMap {
+	[key: string]: string;
+}
+
+/**
+ Object that maps top margin value to their respective classes.
+ 
+ @constant
+ @type {TopMarginMap}
+*/
+const topMarginMap: TopMarginMap = {
+	4: "mt-4",
+	8: "mt-8",
+	12: "mt-12",
+	16: "mt-16",
+	20: "mt-20",
+	24: "mt-24",
+	28: "mt-28",
+	32: "mt-32"
+};
+
+/**
  Props for the TalkList component
 
  @interface TalkListProps
@@ -9,6 +35,7 @@ import { UniformSlot, UniformText } from "@uniformdev/canvas-react";
 */
 export type TalkListProps = {
 	title?: string;
+	marginTop?: number;
 };
 
 /**
@@ -19,9 +46,10 @@ export type TalkListProps = {
  @returns {JSX.Element} - Rendered TalkList component
 */
 export function TalkList(props: TalkListProps): JSX.Element {
+	const marginTopClass = topMarginMap[props.marginTop ?? 0] || "mt-0";
 	return (
 		<fieldset>
-			<section className="bg-white border-b py-8 mt-20">
+			<section className={`bg-white border-b py-8 ${marginTopClass}`}>
 				<div className="container mx-auto flex flex-wrap pt-4 pb-12">
 					<UniformText
 						as="h1"
